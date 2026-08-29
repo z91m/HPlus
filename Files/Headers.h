5759eb570cfc0609e0ade3e97f07942122e32837
z91m <xz.91.mx@gmail.com>
2026-08-29 17:40:13 +0300

    Update Headers.h

--- Files/Headers.h
+++ Files/Headers.h
@@ -230,7 +230,7 @@
 #define RemoveShortsPausedLensButton @"YouModRemoveShortsPausedLensButton"
 #define RemoveShortsPausedTrendsButton @"YouModRemoveShortsPausedTrendsButton"
 #define RemoveShortsDisclosure @"YouModRemoveShortsDisclosure"
-#define HideShortsTitle @"YouModHideShortsTitle"
+#define RemoveShortsTitle @"YouModRemoveeShortsTitle"
 // Tab bar
 #define DefaultTab @"YouModDefaultStartupTab"
 #define TabOrder @"YouModTabOrder"
