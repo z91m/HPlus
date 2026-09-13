95657bcbe00a53553c9443cd7f8533959ea1e711
z91m <xz.91.mx@gmail.com>
2026-09-13 15:07:37 +0300

    Update Headers.h

--- Files/Headers.h
+++ Files/Headers.h
@@ -383,7 +383,16 @@ typedef NS_ENUM(NSUInteger, GestureSection) {
 - (YTVideoFreeZoomOverlayView *)videoFreeZoomOverlayView;
 @end
 
-@interface YTQTMButton (HPlus)
+__attribute__((weak_import))
+@interface YTColor : NSObject
++ (UIColor *)black3;
++ (UIColor *)white1;
+@end
+
+__attribute__((weak_import))
+@interface YTQTMButton : UIButton
++ (instancetype)iconButton;
+- (void)setImage:(UIImage *)image forState:(UIControlState)state;
 - (void)enableNewTouchFeedback;
 @end
 
